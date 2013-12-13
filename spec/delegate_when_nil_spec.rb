f88147aea99c9f06a5db2aec981d68f6e9845ca0
conanite <conan@conandalton.net>
2013-12-13 21:03:52 +0100

    spec: just to make things clear

--- spec/delegate_when_nil_spec.rb
+++ spec/delegate_when_nil_spec.rb
@@ -7,14 +7,20 @@ describe DelegateWhenNil do
 
   class Child
     extend DelegateWhenNil
-    attr_accessor :last_name, :parent
+    attr_accessor :last_name, :parent, :size
     delegate_when_nil :last_name, :to => :parent
+    delegate_when_nil :size, :to => :"parent.last_name"
   end
 
   it "should not intervene when the property is present" do
     child = Child.new
     child.last_name = "Wong"
     expect(child.last_name).to eq "Wong"
+  end
+
+  it "should not get upset when everything is nil" do
+    child = Child.new
+    expect(child.last_name).to eq nil
   end
 
   it "should not intervene when the property is present and the delegate target is also present" do
@@ -29,5 +35,25 @@ describe DelegateWhenNil do
     child.parent = Parent.new
     child.parent.last_name = "Wonka"
     expect(child.last_name).to eq "Wonka"
+  end
+
+  it "should raise an error if a complex delegation target fails" do
+    child = Child.new
+    expect { child.size }.to raise_error
+  end
+
+  it "should execute a complex delegation target as expected" do
+    child = Child.new
+    child.parent = Parent.new
+    child.parent.last_name = "1234567"
+    expect(child.size).to eq 7
+  end
+
+  it "should ignore a complex delegation target as expected when the local property is not nil" do
+    child = Child.new
+    child.size = 21
+    child.parent = Parent.new
+    child.parent.last_name = "1234567"
+    expect(child.size).to eq 21
   end
 end
